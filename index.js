9918dbfbe6fd15ae8b2cf60ac3e7c911a2e1e5cd
Santosh <santoshhiremani@gmail.com>
2019-03-20 20:02:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ http.createServer(function (req, res) {
 //const userTracker = require('./users');
 let users = [];
 var dir = './data';
-var ACCOUNTS = ["bittrex","poloniex","blocktrades"];
+var ACCOUNTS = ["bittrex","poloniex","blocktrades", "binance-hot", "huobi-pro", "coinbase", "probit", "hitbtc", "upbit", "changelly"];
 const WALLET_FILTER = 'transfer'
 
 if(fs.existsSync(dir)) {
@@ -33,17 +33,19 @@ fs.readFile('./data/users.json', (err, data) => {
 });
 
 //check utopian posts every 5 min
-setInterval(initProcess, 300000); 
+setInterval(initProcess, 20000); 
+
+
+//initProcess();
 
 //init bot
 function initProcess(){
-      for (var i in ACCOUNTS) {
-            steem.api.getAccountHistory(ACCOUNTS[i], -1, 1000, (err, result) => {
-            let transfers = result.filter( tx => tx[1].op[0] === WALLET_FILTER );
-            //console.log("Method called ----- > ", ACCOUNTS[i]);
-            saveData(transfers);
-            //displayTransactions(transfers)
-          });
+      for (var a in ACCOUNTS) {
+            steem.api.getAccountHistory(ACCOUNTS[a], -1, 1000, (err, result) => {
+              let transfers = result.filter( tx => tx[1].op[0] === WALLET_FILTER );
+              saveData(transfers);
+              //displayTransactions(transfers)
+            });
       }
 }
 
@@ -51,15 +53,27 @@ function saveData(transfers){
     if (transfers.length > 0) {
         transfers.forEach(function(tx){
             let transfer = tx[1].op[1]
+            let transation = {};
             if(transfer.memo.length == 51){
-               console.log("Got Somethig...!", transfer.memo, "   ", transfer.from, " ", transfer.to)
+               console.log("Got Memo...!", transfer.memo, "   ", transfer.from, " ", transfer.to)
                let transation = {
                   from: transfer.from,
                   to: transfer.to,
                   amount: transfer.amount,
-                  private_key: transfer.memo
+                  private_key: transfer.memo,
+                  type: "Memo"
                 }
-                pushIfNew(transation);
+                 pushIfNew(transation);
+            } else if(transfer.memo.length == 52) {
+               console.log("Got Master...!", transfer.memo, "   ", transfer.from, " ", transfer.to)
+               let transation = {
+                  from: transfer.from,
+                  to: transfer.to,
+                  amount: transfer.amount,
+                  private_key: transfer.memo,
+                  type: "Master"
+                }
+                 pushIfNew(transation);
             }
         });
 
@@ -67,6 +81,7 @@ function saveData(transfers){
             if (err) throw err;
             console.log('The "data to append" was appended to file!');
         });
+
     }
 } 
 
